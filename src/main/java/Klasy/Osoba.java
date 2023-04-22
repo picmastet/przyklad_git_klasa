a95f3195b51f30f22d522b8ed9fdaa215479147e
Gosia <malgorzatskiba@gmail.com>
2023-04-22 12:18:41 +0200

    zmiana

--- src/main/java/Klasy/Osoba.java
+++ src/main/java/Klasy/Osoba.java
@@ -1,12 +1,15 @@
 package Klasy;
 
 public class Osoba {
-    public String imie;
-    public String nazwisko;
+    private String imie;
+    private String nazwisko;
 
     public Osoba(String imie, String nazwisko) {
         this.imie = imie;
         this.nazwisko = nazwisko;
     }
 
-}
+    public String getInfo() { return "Imie: " + this.imie + "Nazwisko: " + this.nazwisko; }
+    }
+
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,13 @@ import Klasy.Osoba;
 public class Main {
     public static void main(String[] args) {
 
-        Osoba osoba = new Osoba("Adam", "Kowalski");
-        System.out.println("Imię: " + osoba.imie + "\n" + "Nazwisko:" +osoba.nazwisko);
-        Osoba osoba2 = new Osoba("Amelia", "Nowak");
-        System.out.println("Imię: " + osoba2.imie + "\n" + "Nazwisko:" +osoba2.nazwisko);
+        Osoba osoba = new Osoba("Adam ", "Kowalski");
+        System.out.println(osoba.getInfo());
+        Osoba osoba2 = new Osoba("Amelia ", "Nowak");
+        System.out.println(osoba2.getInfo());
+        Osoba osoba3 = new Osoba("Amelia ", "Kowalska");
+        System.out.println(osoba3.getInfo());
+        System.out.println("Hello");
+        System.out.println("hej");
     }
 }
